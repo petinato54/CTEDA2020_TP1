a966eaedd2bb7539589007c19328fb5af86fd045
petinato54 <petinato54@gmail.com>
2020-03-06 17:45:00 -0300

    Add files via upload

---
+++ ArbolGeneral.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tp3
+{
+	public class ArbolGeneral<T>
+	{
+		
+		private NodoGeneral<T> raiz;
+
+		public ArbolGeneral(T dato) {
+			this.raiz = new NodoGeneral<T>(dato);
+		}
+	
+		private ArbolGeneral(NodoGeneral<T> nodo) {
+			this.raiz = nodo;
+		}
+	
+		private NodoGeneral<T> getRaiz() {
+			return raiz;
+		}
+	
+		public T getDatoRaiz() {
+			return this.getRaiz().getDato();
+		}
+	
+		public List<ArbolGeneral<T>> getHijos() {
+			List<ArbolGeneral<T>> temp= new List<ArbolGeneral<T>>();
+			foreach (var element in this.raiz.getHijos()) {
+				temp.Add(new ArbolGeneral<T>(element));
+			}
+			return temp;
+		}
+	
+		public void agregarHijo(ArbolGeneral<T> hijo) {
+			this.raiz.getHijos().Add(hijo.getRaiz());
+		}
+	
+		public void eliminarHijo(ArbolGeneral<T> hijo) {
+			this.raiz.getHijos().Remove(hijo.getRaiz());
+		}
+	
+		public bool esVacio() {
+			return this.raiz == null;
+		}
+	
+		public bool esHoja() {
+			return this.raiz != null && this.getHijos().Count == 0;
+		}
+	
+		public int altura() {
+			return 0;
+		}
+	
+		
+		public int nivel(T dato) {
+			return 0;
+		}
+	
+	}
+}
---
+++ Cola.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tp1
+{
+	/// <summary>
+	/// Description of Cola.
+	/// </summary>
+	public class Cola<T>
+	{
+
+		
+		private List<T> datos = new List<T>();
+	
+		public void encolar(T elem) {
+			this.datos.Add(elem);
+		}
+	
+		public T desencolar() {
+			T temp = this.datos[0];
+			this.datos.RemoveAt(0);
+			return temp;
+		}
+		
+		public T tope() {
+			return this.datos[0]; 
+		}
+		
+			public bool esVacia() {
+				return this.datos.Count == 0;
+			}
+		}
+}
